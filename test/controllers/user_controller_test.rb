5f2f6bb8af773df9dbdcdde2b66ac27d7b51d584
Pepe Suarez <pepesuarez@Pepes-Mac-mini.local>
2021-04-01 23:06:26 -0600

    Added initial unauthorized tests

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  #
+  test 'It should return 402 when trying to log in' do
+    params = { username: 'pepe', password: 'worngpass'}
+    post login_url, params: params
+    assert_response :unauthorized
+  end
 end
