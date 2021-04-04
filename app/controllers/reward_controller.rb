b809c8f6218a3c243b8a405a9d4d893a88c5e94c
Pepe Suarez <pepesuarez@Pepes-Mac-mini.local>
2021-04-04 18:14:06 -0500

    Rewards refactor

--- app/controllers/reward_controller.rb
+++ app/controllers/reward_controller.rb
@@ -2,11 +2,8 @@ class RewardController < ApplicationController
   before_action :authorized
 
   def available_rewards
-
     user_reward_manager = RewardManager.find_by_user_id(@user.id)
-    puts "User points", user_reward_manager
-    rewards = Reward.where('price > ?', 200)
-
+    rewards = Reward.where('price < ?', user_reward_manager.points)
     render json: {data: {message:"Available rewards"}, available_rewards:rewards}, status: :ok
   end
 end
